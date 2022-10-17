65f876777d6f27c7d0506129beea0ca1fe598acf
S1GARETA <rabota.dsv@mail.ru>
2022-10-17 21:46:51 +0500

    v 0.0.0.1 / 2

---
+++ DragonPicker/Assets/_Scripts/DragonEgg.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DragonEgg : MonoBehaviour
+{
+    [SerializeField] private static float bottomY = -30;
+    private void OnTriggerEnter(Collider other)
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var em = ps.emission;
+        em.enabled = true;
+
+        Renderer rend;
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;    
+    }
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+            DragonPicker apScript = Camera.main.GetComponent<DragonPicker>();
+            apScript.DragonEggDestroyed();
+        }
+    }
+}
---
+++ DragonPicker/Assets/_Scripts/DragonPicker.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragonPicker : MonoBehaviour
+{
+    [SerializeField] private GameObject energyShieldPrefab;
+    [SerializeField] private int numEnergyShield = 3;
+    [SerializeField] private float energyShieldBottomY = -6f;
+    [SerializeField] private float energyShieldRadius = 1.5f;
+    public List<GameObject> shieldList;
+    void Start()
+    {
+        shieldList = new List<GameObject>();
+
+        for (int i = 1; i <= numEnergyShield; i++)
+        {
+            GameObject tShieldGo = Instantiate<GameObject>(energyShieldPrefab);
+            tShieldGo.transform.position = new Vector3(0, energyShieldBottomY, 0);
+            tShieldGo.transform.localScale = new Vector3(1*i, 1*i, 1*i);
+            shieldList.Add(tShieldGo);
+        }
+    }
+
+    public void DragonEggDestroyed()
+    {
+        GameObject[] tDragonEggArray = GameObject.FindGameObjectsWithTag("Dragon Egg");
+        foreach (GameObject tGO in tDragonEggArray)
+        {
+            Destroy(tGO);
+        }
+        int shieldIndex = shieldList.Count - 1;
+        GameObject tShieldGo = shieldList[shieldIndex];
+        shieldList.RemoveAt(shieldIndex);
+        Destroy(tShieldGo);
+
+        if (shieldList.Count == 0)
+        {
+            SceneManager.LoadScene("_0Scene");
+        }
+    }
+}
